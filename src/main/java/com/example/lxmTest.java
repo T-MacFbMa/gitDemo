8161848ed47d04a141aff476d60b1059b10760e7
yangmei <yangm@daqsoft.com>
2017-02-19 20:26:31 +0800

    lxm first commit test

---
+++ src/main/java/com/example/lxmTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by yangmei on 17/2/19.
+ */
+public class lxmTest {
+    public static void main(String[] args) {
+        System.out.println("2222222");
+    }
+}
